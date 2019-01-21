63e17b6d41ca7273d11ede3e82cf6714b889d9c7
Noah Johnson <noah.johnson@visionsystemsinc.com>
2019-01-21 18:08:44 -0500

    simplified the entire tree into one class. got a simple iterator working, and used it for brute force nearest neighbor search

--- kd_tree.h
+++ kd_tree.h
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
+#include <limits>
+#include <numeric>
 #include <vector>
 
 // template < class T, std::size_t dims >
@@ -33,27 +35,62 @@
 //     T data_[dims];  // fixed-length array to hold data
 // };
 
-template <class T>
-class Node {
+template <class T, int k>
+class KDTree {
 
   private:
-    Node<T>* left_child_;
-    Node<T>* right_child_;
+    KDTree<T,k>* left_child_;
+    KDTree<T,k>* right_child_;
+    KDTree<T,k>* parent_;
     std::vector<T> split_point_;
+    int split_axis_;
 
   public:
 
-    Node(std::vector<T> split_point, Node<T>* left_child, Node<T>* right_child):
-      split_point_(split_point), left_child_(left_child), right_child_(right_child) {}
+    KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typename std::vector<std::vector<T>>::iterator points_end,
+           int depth=0) {
 
-    std::vector<T> const split_point() {
-      return split_point_;
+      // Special case for root
+      if (depth == 0) {
+        parent_ = nullptr;
+      }
+
+      // Cycle through axes to split on
+      int axis = depth % k;
+
+      // Sort points along this axis using a lambda comparator
+      std::sort(points_begin, points_end, [axis](const std::vector<T>& a, const std::vector<T>& b) {
+                return a[axis] < b[axis];
+                });
+
+      // select median point along this axis
+      typename std::vector<std::vector<T>>::iterator median_it = std::next(points_begin, std::distance(points_begin, points_end) / 2);
+
+      // split on that median point
+      split_point_ = *(median_it);
+
+      // Recurse on sub-arrays of points to the left and right of median point
+      if (std::distance(points_begin, median_it) <= 0) {
+        left_child_ = nullptr;
+      } else {
+        left_child_ = new KDTree<T,k>(points_begin, median_it, depth + 1);
+        left_child_->setParent(this);
+      }
+      if (std::distance(std::next(median_it), points_end) <= 0) {
+        left_child_ = nullptr;
+      } else {
+        right_child_ = new KDTree<T,k>(std::next(median_it), points_end, depth + 1);
+        right_child_->setParent(this);
+      }
     }
-    Node<T>* const left_child() {
-      return left_child_;
-    }
-    Node<T>* const right_child() {
-      return right_child_;
+
+    /*KDTree(std::vector<T> split_point, KDTree<T>* left_child, KDTree<T>* right_child, int split_axis):
+      split_point_(split_point), left_child_(left_child), right_child_(right_child), split_axis_(split_axis) {}*/
+
+    ~KDTree() {
+      // recursively delete children
+      delete left_child_;
+      delete right_child_;
     }
 
     void const prettyPrint(int indent=0) {
@@ -67,7 +104,7 @@ class Node {
       }
       if (right_child_) std::cout << " /\n" << std::setw(indent) << ' ';
 
-      // Print root point
+      // Print split point
       std::cout << "( ";
       for (auto el : split_point_) {
         std::cout << el << ' ';
@@ -82,43 +119,166 @@ class Node {
     }
 
     class iterator {
-      private:
-        Node<T>* p_;
       public:
-        iterator() {}
-        iterator(const iterator&) {};
-        std::vector<T>& operator* () {
-          return p_->split_point();  // dereferencing will return the point at the current node
+        KDTree<T,k>* node_ptr_;
+
+        iterator(): node_ptr_(nullptr) {} // empty constructor
+        iterator(KDTree<T,k>* node) { // constructor which takes a KD-Tree pointer to root
+          while (node->left_child() || node->right_child()) {
+            if (node->left_child()) {
+              node = node->left_child();
+            } else {
+              node = node->right_child();
+            }
+          }
+          node_ptr_ = node;
         }
-        iterator& operator++ () {
-          p_ = p_;
+        iterator(const iterator& other) {
+          node_ptr_ = other.node_ptr_;
+        };
+        std::vector<T> operator* () {
+          return node_ptr_->split_point();  // dereferencing will return the point at the current node
+        }
+        iterator& operator++ () { // prefix ++
+          if (node_ptr_->right_child()) { // go to the right child and find the leftmost child node
+            node_ptr_ = node_ptr_->right_child();
+            while (node_ptr_->left_child()) {
+              node_ptr_ = node_ptr_->left_child();
+            }
+          } else { // if there is no right child, go up until one exists or we hit the null parent above root
+            KDTree<T,k>* parent = node_ptr_->parent();
+            while (parent && parent->right_child() == node_ptr_) {
+              node_ptr_ = parent;
+              parent = parent->parent();
+            }
+            if (!parent || node_ptr_->right_child() != parent) {
+              node_ptr_ = parent;
+            }
+          }
           return *this;  // return reference to this iterator
         }
-        iterator operator++ (int) {
+        iterator operator++ (int) { // postfix ++
           iterator tmp = *this;
           ++(*this);
           return tmp;
         }
         bool operator== (const iterator& other) const {
-          return p_ == other.p_; // TODO: Fix
+          return node_ptr_ == other.node_ptr_; // TODO: Fix
         }
         bool operator!= (const iterator& other) const {
-          return p_ != other.p_; // TODO: Fix
+          return node_ptr_ != other.node_ptr_; // TODO: Fix
         }
     };
 
     iterator begin() {
       return iterator(this);
     }
-    iterator end() {
+    iterator begin() const {
+      // TODO
+      ;
+      //return const_iterator(this);
+    }
+    iterator end() const {
       return iterator();
+    }
+
+    // Setters
+    void setParent(KDTree<T,k>* parent) {
+      parent_ = parent;
+    }
+
+    // Getters
+    KDTree<T,k>* left_child() const {
+      return left_child_;
+    }
+    KDTree<T,k>* right_child() const {
+      return right_child_;
+    }
+    KDTree<T,k>* parent() const {
+      return parent_;
+    }
+    std::vector<T> split_point() const {
+      return split_point_;
+    }
+    int split_axis() const {
+      return split_axis_;
     }
 
 };
 
+template <class T, int k>
+std::vector<T> findNN_brute_force(KDTree<T,k> & tree, std::vector<T> const& ref_point) {
+  // Brute force search
 
-template <class T>
-Node<T>* KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typename std::vector<std::vector<T>>::iterator points_end, int depth=0) {
+  std::vector<T> current_best;
+  auto current_best_dist = std::numeric_limits<int>::max();
+
+  for (typename KDTree<T,k>::iterator it = tree.begin(); it != tree.end(); ++it) {
+    std::vector<T> point = *it;
+
+    auto dist = std::inner_product(point.begin(), point.end(), ref_point.begin(),
+                                   T(0), std::plus<T>(), [](T x, T y) {return (y-x)*(y-x);});
+    if (dist < current_best_dist) {
+      current_best = point;
+      current_best_dist = dist;
+    }
+  }
+  return current_best;
+
+
+  /*
+  std::vector<T> current_best = split_point_;
+
+  std::vector<T> left_best;
+  if (left_child_) left_best = left_child_->findNN(ref_point);
+
+  std::vector<T> right_best;
+  if (right_child_) right_best = right_child_->findNN(ref_point);
+  */
+
+}
+
+/*
+
+template <class T, int k>
+class KDTree {
+  private:
+    Node<T> root;
+  public:
+    KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typename std::vector<std::vector<T>>::iterator points_end) {
+      // Sort points along this axis using a lambda comparator
+      std::sort(points_begin, points_end, [axis](const std::vector<T>& a, const std::vector<T>& b) {
+                return a[axis] < b[axis];
+                });
+
+      // Select median point along this axis
+      typename std::vector<std::vector<T>>::iterator median_it = std::next(points_begin, std::distance(points_begin, points_end) / 2);
+      std::vector<T> median_point = *(median_it);
+
+      // Recurse on sub-arrays of points to the left and right of median point
+      Node<T>* leftChild = KDTree<T>(points_begin, median_it, depth + 1);
+      Node<T>* rightChild = KDTree<T>(std::next(median_it), points_end, depth + 1);
+
+      // After sub-trees have been built, construct and return this split point node
+      // This memory will persist outside of this function, and will need to be deleted eventually
+      // to avoid memory leaks
+      root = new Node<T>(median_point, leftChild, rightChild);
+    }
+
+    Node<T>* createTree(typename std::vector<std::vector<T>>::iterator points_begin,
+                        typename std::vector<std::vector<T>>::iterator points_end,
+                        int depth=0) {
+      d
+    }
+    std::vector<T> findNN(std::vector<T> ref) {
+      std::vector<T> current_best;
+    }
+
+}; */
+
+
+//template <class T>
+//Node<T>* KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typename std::vector<std::vector<T>>::iterator points_end, int depth=0) {
   /* Recursively construct a k-d tree from a set of points
   From: https://en.wikipedia.org/wiki/K-d_tree#Construction
   Params:
@@ -129,7 +289,7 @@ Node<T>* KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typ
   Returns:
     Root of tree
   */
-
+/*
   // Handle base case
   if (std::distance(points_begin, points_end) <= 0) {
     return nullptr;
@@ -140,7 +300,7 @@ Node<T>* KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typ
 
   // Cycle through axes to split on
   int axis = depth % k;
-
+*/
   /*
   std::vector<std::vector<T>> cp(points_begin, points_end);
   std::sort(cp.begin(), cp.end(), [axis](const std::vector<T>& a, const std::vector<T>& b) {
@@ -154,7 +314,7 @@ Node<T>* KDTree(typename std::vector<std::vector<T>>::iterator points_begin, typ
   Node<T>* leftChild = KDTree<T>(cp.begin(), median_it, depth + 1);
   Node<T>* rightChild = KDTree<T>(std::next(median_it), cp.end(), depth + 1);
   */
-
+/*
   // Sort points along this axis using a lambda comparator
   std::sort(points_begin, points_end, [axis](const std::vector<T>& a, const std::vector<T>& b) {
             return a[axis] < b[axis];
@@ -196,5 +356,6 @@ void deleteTree(Node<T>* root) {
 
   return;
 }
+*/
 
 #endif
--- nearest_neighbor.cpp
+++ nearest_neighbor.cpp
@@ -4,8 +4,10 @@
 
 int main()
 {
+  // use vector of vectors to represent a dynamic-sized array of dynamic-sized points
   std::vector<std::vector<int>> points = { {2,3}, {5,4}, {9,6}, {4,7}, {8,1}, {7,2} };
 
+  // print out the points
   std::cout << "Input list of points:" << std::endl;
   for (auto i=points.begin(); i!= points.end(); ++i) {
     std::cout << "( ";
@@ -15,11 +17,37 @@ int main()
     std::cout << ")" << std::endl;
   }
 
-  Node<int>* tree = KDTree<int>(std::begin(points), std::end(points));
+  // construct KD Tree from points
+  KDTree<int, 2> tree = KDTree<int,2>(std::begin(points), std::end(points));
 
+  // print out the structure of the tree
   std::cout << std::endl << "K-D Tree:" << std::endl;
-  tree->prettyPrint();
+  tree.prettyPrint();
 
-  deleteTree(tree);
+  // arbitrary point that we will search for nearest neighbors of
+  int x,y;
+  std::cout << "Please enter a point to do a nearest neighbor search on:" << std::endl << "x: ";
+  std::cin >> x;
+  std::cout << "y: ";
+  std::cin >> y;
+
+  std::vector<int> ref_point {x,y};
+
+  // run a nearest neighbor search using the kd-tree
+  std::vector<int> NN = findNN_brute_force(tree, ref_point);
+
+  std::cout << std::endl << "Nearest neighbor to reference point (";
+  for (auto i : ref_point) {
+    std::cout << i << ' ';
+  }
+  std::cout << ") is (";
+  for (auto i : NN) {
+    std::cout << i << ' ';
+  }
+  std::cout << ")" << std::endl;
+
+  // test deletor
+  //delete &tree;
+
   return 0;
 }
